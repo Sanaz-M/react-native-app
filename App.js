c8e7f41e9a1c6e8fd726e57aeaff6ceef9b5a21c
Sanaz M <sanazmohammaddi@gmail.com>
2021-12-09 21:19:55 +0000

    add a photo

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontSize: 16 }}>My first <Text style={{ fontWeight: 'bold' }}>native app 
+      </Text> with React Native</Text>
       <StatusBar style="auto" />
+      <Image
+        style={{
+          width: 200,
+          height: 200,
+        }}
+        source={
+          require('./assets/test.png')}
+      />
     </View>
   );
 }
